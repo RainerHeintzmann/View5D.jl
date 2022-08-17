1fd252fe0804e380ed9fe2c20ef37d6a49ce7cab
RainerHeintzmann <heintzmann@gmail.com>
2022-08-17 09:50:45 +0200

    updated GetArtifactInfo.jl

--- src/GetArtifactInfo.jl
+++ src/GetArtifactInfo.jl
@@ -1,4 +1,4 @@
 using Tar, Inflate, SHA    
-filename="C:\\Users\\pi96doc\\Documents\\Programming\\Java\\View5D\\View5D_v2.3.2-jar.tar.gz"
+filename="C:\\Users\\pi96doc\\Documents\\Programming\\Java\\View5D\\View5D_v2.3.8-jar.tar.gz"
 println("git-tree-sha1 = \"", Tar.tree_hash(IOBuffer(inflate_gzip(filename))),"\"")
 println("sha256 = \"", bytes2hex(open(sha256, filename)), "\"")
