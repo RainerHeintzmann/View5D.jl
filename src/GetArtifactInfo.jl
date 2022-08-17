4da2821bd8a4cd989920c4f49f77a8ca91ae454d
RainerHeintzmann <heintzmann@gmail.com>
2022-08-17 09:55:57 +0200

    put new version numbers and SHA into the Artifacts.toml file

--- src/GetArtifactInfo.jl
+++ src/GetArtifactInfo.jl
@@ -2,3 +2,4 @@ using Tar, Inflate, SHA
 filename="C:\\Users\\pi96doc\\Documents\\Programming\\Java\\View5D\\View5D_v2.3.8-jar.tar.gz"
 println("git-tree-sha1 = \"", Tar.tree_hash(IOBuffer(inflate_gzip(filename))),"\"")
 println("sha256 = \"", bytes2hex(open(sha256, filename)), "\"")
+# and then put these values into the Artifacts.toml file and also update the Version number there.
