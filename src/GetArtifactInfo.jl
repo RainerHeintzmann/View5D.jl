759461dea6575c0d66f6af8fa83f0e0c9792e2b4
RainerHeintzmann <heintzmann@gmail.com>
2024-11-27 09:19:40 +0100

    updated Asset path

--- src/GetArtifactInfo.jl
+++ src/GetArtifactInfo.jl
@@ -1,11 +1,10 @@
 using Tar, Inflate, SHA
 filename="C:\\Users\\pi96doc\\Documents\\Programming\\Java\\View5D\\target\\View5D_-$V.$VV.$VVV$Suffix.tar.gz"
-
 println("[View5D-jar]")
 println("git-tree-sha1 = \"", Tar.tree_hash(IOBuffer(inflate_gzip(filename))),"\"")
 println("lazy = true\n")
 println("    [[View5D-jar.download]]")
-println("    url = \"https://github.com/RainerHeintzmann/View5D/releases/download/View5D_-$(V).$VV.$VVV/View5D_v$V.$VV.$VVV.tar.gz\" ")
+println("    url = \"https://github.com/bionanoimaging/View5D/releases/download/View5D_v$(V).$VV.$VVV/View5D_-$V.$VV.$VVV$Suffix.tar.gz\" ")
 println("    sha256 = \"", bytes2hex(open(sha256, filename)), "\"")
 
 # and then put these values into the Artifacts.toml file and also update the Version number there.
