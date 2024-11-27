d66c24866ee3fd52d4ef496ab216098432c28508
RainerHeintzmann <heintzmann@gmail.com>
2024-11-27 08:43:08 +0100

    updated Version number and Artifacts.toml

--- src/GetArtifactInfo.jl
+++ src/GetArtifactInfo.jl
@@ -1,7 +1,11 @@
 using Tar, Inflate, SHA
-filename="C:\\Users\\pi96doc\\Documents\\Programming\\Java\\View5D\\View5D_v$V.$VV.$VVV-jar.tar.gz"
+filename="C:\\Users\\pi96doc\\Documents\\Programming\\Java\\View5D\\target\\View5D_-$V.$VV.$VVV$Suffix.tar.gz"
+
+println("[View5D-jar]")
 println("git-tree-sha1 = \"", Tar.tree_hash(IOBuffer(inflate_gzip(filename))),"\"")
-println("url = \"https://github.com/RainerHeintzmann/View5D/releases/download/View5D_v$(V).$VV.$VVV/View5D_v$V.$VV.$VVV-jar.tar.gz\" ")
-println("sha256 = \"", bytes2hex(open(sha256, filename)), "\"")
+println("lazy = true\n")
+println("    [[View5D-jar.download]]")
+println("    url = \"https://github.com/RainerHeintzmann/View5D/releases/download/View5D_-$(V).$VV.$VVV/View5D_v$V.$VV.$VVV.tar.gz\" ")
+println("    sha256 = \"", bytes2hex(open(sha256, filename)), "\"")
 
 # and then put these values into the Artifacts.toml file and also update the Version number there.
--- test/runtests.jl
+++ test/runtests.jl
@@ -94,10 +94,11 @@ end
     v = view5d(rand(Float32,2,2,2,2,3))
     @test 3 == get_num_times(v)
     @test 2 == get_num_elements(v)
-
+    @test nothing == close_all()
 end
 
 @testset "set functions" begin
     v = view5d(rand(Float32,2,2,2,2,3))
-    set_colormap_no(13, v)
+    @test set_colormap_no(13, nothing, v) == nothing
+    @test nothing == close_all()
 end
