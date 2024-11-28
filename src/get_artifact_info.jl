a37d3896788ecf57b25bf7b3a4fe874ee582324e
RainerHeintzmann <heintzmann@gmail.com>
2024-11-28 12:06:11 +0100

    fixed the wrong artifact path issue

--- src/get_artifact_info.jl
+++ src/get_artifact_info.jl
@@ -1,10 +1,14 @@
 using Tar, Inflate, SHA
-filename="C:\\Users\\pi96doc\\Documents\\Programming\\Java\\View5D\\target\\View5D_-$V.$VV.$VVV$Suffix-jar.tar.gz"
+fn = "View5D_-$V.$VV.$VVV$Suffix-jar"
+filename="C:\\Users\\pi96doc\\Documents\\Programming\\Java\\View5D\\target\\$(fn).tar.gz"
 println("[View5D-jar]")
 println("git-tree-sha1 = \"", Tar.tree_hash(IOBuffer(inflate_gzip(filename))),"\"")
 println("lazy = true\n")
 println("    [[View5D-jar.download]]")
-println("    url = \"https://github.com/bionanoimaging/View5D/releases/download/View5D_v$(V).$VV.$VVV/View5D_-$V.$VV.$VVV$Suffix-jar.tar.gz\" ")
+println("    url = \"https://github.com/bionanoimaging/View5D/releases/download/View5D_v$(V).$VV.$VVV/$(fn).tar.gz\" ")
 println("    sha256 = \"", bytes2hex(open(sha256, filename)), "\"")
+println("\n\n#Put this into libne 40 of viewer_core:")
+fn = "View5D_-$V.$VV.$VVV$Suffix.jar"
+println("const View5D_jar = joinpath(rootpath, \"$(fn)\")")
 
 # and then put these values into the Artifacts.toml file and also update the Version number there.
